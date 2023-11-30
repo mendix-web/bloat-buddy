901cf14f234b87e2270f6e610155f09902cdbb93
matteszk <mszklarz@gmail.com>
2023-11-30 16:11:08 -0500

    add email when over limit, with 3 day reminder

--- classes/bloat-buddy.php
+++ classes/bloat-buddy.php
@@ -2,25 +2,17 @@
 /**
  * The core plugin class.
  *
- * @since      1.0.0
+ * @since      1.0.1
  * @package    Bloat_Buddy
  * @subpackage Bloat_Buddy/includes
  * @author     Matthew Szklarz <mszklarz@gmail.com>
  */
 class Bloat_Buddy {
-	/**
-	 * Undocumented variable
-	 *
-	 * @var integer
-	 */
-	private int $mb_limit_autoload_single = 800000;
 
-	/**
-	 * The warning size for the wp_options table
-	 *
-	 * @var integer
-	 */
-	private int $mb_limit_autoload_total = 1800000;
+	const BYTE_LIMIT_SINGLE      = 800000; // Warning limit for single options.
+	const BYTE_LIMIT_TOTAL       = 1800000; // Warning limit for total autoload wp_options.
+	const FREQ_NOTICE_EMAIL_DAYS = 3; // Frequency at which notice emails should be sent.
+	const SECONDS_PER_DAY        = 86400; // Amount of seconds in a day.
 
 	/**
 	 * Undocumented function
@@ -36,7 +28,7 @@ class Bloat_Buddy {
 	 */
 	public function on_wp_dashboard_setup() {
 		wp_add_dashboard_widget(
-			'wp_options_bloat', // $widget_id: an identifying slug for your widget. This will be used as its CSS class and its key in the array of widgets.
+			'wp_options_bloat_buddy', // $widget_id: an identifying slug for your widget. This will be used as its CSS class and its key in the array of widgets.
 			'Bloat Buddy', // $widget_name: this is the name your widget will display in its heading.
 			array( $this, 'buddy_output_widget' ), // $callback: The name of a function you will create that will display the actual contents of your widget.
 			array( $this, 'buddy_handle_widget' ), // $control_callback (Optional): The name of a function you create that will handle submission of widget options forms, and will also display the form elements.
@@ -59,40 +51,52 @@ class Bloat_Buddy {
 		if ( $results ) {
 			echo '<h3><b>Total Autoload Options Size</b></h3>';
 			if ( $results ) {
-				$bytes = $results[0]->value;
+				$bytes            = $results[0]->value;
+				$bytes_over_limit = intval( $bytes ) > self::BYTE_LIMIT_TOTAL;
+				$is_over_limit    = $bytes_over_limit > 0;
+
 				printf(
 					'<p style="color: %s">%s bytes (%s)</p>',
-					intval( $bytes ) > $this->mb_limit_autoload_total ? 'red' : 'inherit',
+					$is_over_limit ? 'red' : 'inherit',
 					$bytes,
 					$this->format_bytes( $bytes ),
 				);
+
+				if ( $is_over_limit ) {
+					$wpobb_email_dt = get_option( 'wpobb_email_dt' ) ?? 0;
+					$diff_dt        = time() - $wpobb_email_dt;
+					$diff_days      = $diff_dt / self::SECONDS_PER_DAY;
+
+					if ( $diff_days > self::FREQ_NOTICE_EMAIL_DAYS ) {
+						$this->send_email_notice( $bytes_over_limit );
+					}
+				}
 			}
 		}
 
-		// Display  10 biggest autoload options.
+		// Display 10 biggest autoload options.
 		$results = $wpdb->get_results(
 			"SELECT LENGTH(option_value),option_name FROM wp_options WHERE autoload='yes' ORDER BY length(option_value) DESC LIMIT 10;"
 		);
-		if ( $results ) {
-			if ( is_array( $results ) ) {
-				echo '<hr style="margin-top: 2rem">';
-				echo '<h3><b>Autoload Options Leaderboard</b></h3>';
-				echo '<ul>';
-				foreach ( $results as  $r ) {
-					$r     = (array) $r;
-					$bytes = $r['LENGTH(option_value)'];
-					printf(
-						'<li><b style="color: %s">%s</b> %s bytes (%s)',
-						intval( $bytes ) > $this->mb_limit_autoload_single ? 'red' : 'inherit',
-						esc_html( $r['option_name'] ),
-						esc_html( $bytes ),
-						esc_html( $this->format_bytes( $bytes ) ),
-					);
-				}
-				echo '</ul>';
+		if ( is_array( $results ) && count( $results ) ) {
+			echo '<hr style="margin-top: 2rem">';
+			echo '<h3><b>Autoload Options Leaderboard</b></h3>';
+			echo '<ul>';
+			foreach ( $results as  $r ) {
+				$r     = (array) $r;
+				$bytes = $r['LENGTH(option_value)'];
+				printf(
+					'<li><b style="color: %s">%s</b> %s bytes (%s)',
+					intval( $bytes ) > self::BYTE_LIMIT_SINGLE ? 'red' : 'inherit',
+					esc_html( $r['option_name'] ),
+					esc_html( $bytes ),
+					esc_html( $this->format_bytes( $bytes ) ),
+				);
 			}
+			echo '</ul>';
 		}
 
+		// TODO add email send button?
 		$user_meta = get_userdata( get_current_user_id() );
 		if ( in_array( 'administrator', $user_meta->roles, true ) ) {
 			// Output buttons to perform admin actions here.
@@ -106,6 +110,37 @@ class Bloat_Buddy {
 	 */
 	public function buddy_handle_widget() {
 		// Handle admin actions here.
+	}
+
+	/**
+	 * Send an email to the specified user (or site admin) about the limit size.
+	 *
+	 * @param integer $bytes_over_limit Bytes over specified limit size
+	 * @return void
+	 */
+	private function send_email_notice( int $bytes_over_limit ) {
+		$to = get_option( 'admin_email' );
+		if ( $to ) {
+			update_option( 'wpobb_email_dt', time() );
+
+			$site_name = get_bloginfo();
+			$formatted = $this->format_bytes( $bytes_over_limit );
+
+			$from    = 'bloat.buddy@mendix.com';
+			$subject = "$site_name wp_options table is $formatted over the size limit";
+			$body    = 'I’m Bloat Buddy and I’m here to say / your database needs cleanup to-day 🎤 🫳';
+
+			$headers = implode(
+				"\r\n",
+				array(
+					"From: $from",
+					"Reply-To: $from",
+					'X-Mailer: PHP/' . PHP_VERSION,
+					'Content-Type: text/html; charset=UTF-8',
+				)
+			);
+			wp_mail( $to, $subject, $body, $headers );
+		}
 	}
 
 	/**
